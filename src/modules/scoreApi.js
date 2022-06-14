37d41723d2753912dd40af9df0cc5ab2e7caab96
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-06-14 19:49:23 +0300

    scoreApi.js

--- src/modules/scoreApi.js
+++ src/modules/scoreApi.js
@@ -1,13 +1,46 @@
-const scoreTable = document.querySelector('.score-table');
-const form = document.querySelector('.form');
-const refresh = document.querySelector('.refresh-button');
-const nameField = document.querySelector('#names');
-const scoreField = document.querySelector('#marks');
+class Score {
+  constructor(user, score) {
+    this.user = user;
+    this.score = score;
+  }
 
-export {
-  scoreTable,
-  refresh,
-  nameField,
-  scoreField,
-  form,
-};
+    scoreData = [];
+
+    url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/:id/scores/:id';
+
+    getScore = () => {
+      const recentScore = document.querySelector('.recent-score-group');
+      recentScore.innerHTML = this.scoreData.map((element, index) => `<li class=${index % 2 !== 0 ? 'list-gray' : 'list-white'}>${element.user} : ${element.score}</li>`).join('');
+    }
+
+    fetchScore = async () => {
+      try {
+        const data = await fetch(this.baseUrl);
+        const res = await data.json();
+        const displayData = res.result.map((item) => item);
+        this.scoreData = displayData;
+        return this.getScore();
+      } catch (err) { return err; }
+    };
+
+    addScore = async ({ user, scoreNum }) => {
+      try {
+        const newgame = { user, score: scoreNum };
+        const config = {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newgame),
+        };
+
+        const data = await fetch(this.url, config);
+        this.scoreData.push(newgame);
+        this.getScore();
+        return data;
+      } catch (err) { return err; }
+    }
+}
+
+export default Score;
