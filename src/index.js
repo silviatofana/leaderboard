80d432598e8353e353dd538d5b31b4293c6d46e2
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-06-14 23:07:06 +0300

    games

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
-// css files here
-import './style.css';
-import dummy from './modules/dummy.js';
-import Score from './modules/scoreApi.js';
+import './index.css';
+import { addGameScore, displayScore } from './apex.js';
 
-const newScore = new Score();
-const addNewScore = document.querySelector('.add-new-score');
-addNewScore.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const user = addNewScore.name.value;
-  const scoreNum = addNewScore.score.value;
-  newScore.addScore({ user, scoreNum });
-  addNewScore.name.value = '';
-  addNewScore.score.value = '';
+displayScore();
+
+const form = document.querySelector('.form');
+const refresh = document.getElementById('recent-scores-button');
+
+refresh.addEventListener('click', () => {
+  displayScore();
 });
 
-const refreshButton = document.querySelector('.refresh-button');
-refreshButton.addEventListener('click', newScore.fetchScore);
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const userscore = document.getElementById('score').value;
+
+  if (name !== '' && userscore !== '') {
+    const data = {
+      user: name,
+      score: userscore,
+    };
+    addGameScore(data);
+    form.reset();
+  }
+});
