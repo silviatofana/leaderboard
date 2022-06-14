4fd37ea0b43635a012eaa856ca11b6d93a918a0a
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-06-14 23:10:54 +0300

    upadtes

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,24 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './docs',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'docs'),
-    clean: true,
-  },
-  optimization: {
-    runtimeChunk: 'single',
-  },
-  module: {
-    rules: [{
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      type: 'asset/resource',
-    },
-    {
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: 'asset/resource',
-    },
-    ],
-  },
 };
