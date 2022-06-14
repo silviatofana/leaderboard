73530950e31dc690135469668922267f9273e2ba
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-06-14 17:10:42 +0300

    setup-project

---
+++ src/modules/tools.js
@@ -0,0 +1,13 @@
+const scoreTable = document.querySelector('.score-table');
+const form = document.querySelector('.form');
+const refresh = document.querySelector('.refresh-button');
+const nameField = document.querySelector('#names');
+const scoreField = document.querySelector('#marks');
+
+export {
+  scoreTable,
+  refresh,
+  nameField,
+  scoreField,
+  form,
+};
