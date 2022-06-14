0b02fdcc705b62c351729894930ed90d1c816f65
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-06-14 17:12:33 +0300

    setup-project

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
 // css files here
 import './style.css';
+import LeaderBoard from './modules/add.js';
+import * as Tools from './modules/tools.js';
+
+const freshScore = new LeaderBoard();
+
+const showScore = (scores) => {
+  let details = '';
+  scores.score.forEach((score) => {
+    details += `
+        <li>${score.iName}   for   ${score.iScore}</li>`;
+  });
+  Tools.scoreTable.innerHTML = details;
+};
+showScore(freshScore);
+
+Tools.form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const iName = Tools.form.names.value;
+  const iScore = Tools.form.marks.value;
+
+  if (iName === '' && iScore === '') {
+    freshScore.addScore(iName, iScore);
+    freshScore.updateScore();
+    showScore(freshScore);
+    Tools.form.tools.names.value = '';
+    Tools.form.tools.marks.value = '';
+  }
+});
+
+showScore(freshScore);
